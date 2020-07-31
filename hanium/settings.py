55ece9a260e235cfc0c200c46e0ed59223f2ce01
2018029416 <nmj5012@naver.com>
2020-07-31 19:56:40 +0900

    test5

--- hanium/settings.py
+++ hanium/settings.py
@@ -100,9 +100,7 @@ DATABASES = {
         'PASSWORD': 'insta2020',
         'HOST': 'hanium-travel-db.cbgr8gnptxaq.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
-    }
+
 }
 }
 db_from_env = dj_database_url.config(conn_max_age = 500)
