b1546323f957f3d73c7ba089ae2d5e50e7238b67
2018029416 <nmj5012@naver.com>
2020-07-31 19:59:14 +0900

    test6

--- hanium/settings.py
+++ hanium/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!09bm96vmtbjt4)sj*i_j90^771i%m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
